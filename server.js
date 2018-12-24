e455a1a9075b423626935532c433e0edb7f59741
StanislavPetkov <sunny@sunny78.net>
2018-12-24 13:48:44 +0200

    /streams added

--- server.js
+++ server.js
@@ -177,7 +177,7 @@ app.get('/streams', function (req, res) {
     //url: "http://"+localip+":3000/hls/playlist.m3u8"
     const streams = {
         version: "1.0",
-        url: "http://"+localip+":3000/hls/playlist.m3u8&rip="+ip;
+        url: "http://"+localip+":3000/hls/playlist.m3u8&rip="+ip
     };
 
     res.send(streams);
