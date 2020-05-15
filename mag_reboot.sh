346b649f76a2d8ebda77241b79dc92e8be970d39
stanislavpetkov <sunny@sunny78.net>
2020-05-15 16:28:56 +0300

    /streams added

--- mag_reboot.sh
+++ mag_reboot.sh
@@ -1,2 +1,2 @@
-sshpass -f <(printf '%s\n' 930920)  ssh root@"$1" "LD_LIBRARY_PATH=/lib:/usr/local/lib:/usr/local/share/app/lib:/usr/local/n/bin:/sbin /sbin/reboot"
+sshpass -p '930920'  ssh root@"$1" "LD_LIBRARY_PATH=/lib:/usr/local/lib:/usr/local/share/app/lib:/usr/local/n/bin:/sbin /sbin/reboot"
 
